b2ec03390eb336f6d626dd63d8a2609c2bb10354
Marcel Martino <marcelmartino2053@gmail.com>
2023-02-07 12:43:55 +0100

    Modify url to insert page number add function to change page number

--- src/store/contexts/games/GameContextProvider.tsx
+++ src/store/contexts/games/GameContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import gamesReducer from "../../reducers/gamesReducer";
 import GamesContext from "./GameContext";
 
@@ -8,9 +8,23 @@ interface GamesContextProviderProps {
 
 const GamesContextProvider = ({ children }: GamesContextProviderProps) => {
   const [games, dispatch] = useReducer(gamesReducer, []);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const nextPage = (): void => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  const previousPage = (): void => {
+    if (pageNumber === 1) {
+      return;
+    }
+    setPageNumber(pageNumber - 1);
+  };
 
   return (
-    <GamesContext.Provider value={{ games, dispatch }}>
+    <GamesContext.Provider
+      value={{ games, dispatch, nextPage, previousPage, pageNumber }}
+    >
       {children}
     </GamesContext.Provider>
   );
