57a463bd044cab39729ef5d6c9cd6c3e9649d29a
Marcel Martino <marcelmartino2053@gmail.com>
2023-02-09 21:41:19 +0100

    Add missing genres to dropdown list

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -23,9 +23,25 @@ const FilterButton = (): JSX.Element => {
     <div className="filterByGenre__button">
       <select onChange={loadGamesByGenre} value={selectedGenre}>
         <option value="">Most Popular</option>
-        <option value="action">action</option>
-        <option value="indie">indie</option>
-        <option value="adventure">adventure</option>
+        <option value="action">Action</option>
+        <option value="indie">Indie</option>
+        <option value="adventure">Adventure</option>
+        <option value="role-playing-games-rpg">RPG</option>
+        <option value="strategy">Strategy</option>
+        <option value="shooter">Shooter</option>
+        <option value="casual">Casual</option>
+        <option value="simulation">Simulation</option>
+        <option value="puzzle">Puzzle</option>
+        <option value="arcade">Arcade</option>
+        <option value="platformer">Platformer</option>
+        <option value="racing">Racing</option>
+        <option value="massively-multiplayer">Massively-multiplayer</option>
+        <option value="sports">Sports</option>
+        <option value="fighting">Fighting</option>
+        <option value="family">Family</option>
+        <option value="board-games">Board Games</option>
+        <option value="educational">Educational</option>
+        <option value="card">Card</option>
       </select>
     </div>
   );
