eb576887059d805367dbe37f6f68095b7f5ee1b0
Dani Setien <dani.setien@gmail.com>
2023-02-10 11:08:11 +0100

    Add Browser component

--- src/components/GameCardList/GameCardListStyled.tsx
+++ src/components/GameCardList/GameCardListStyled.tsx
@@ -1,25 +1,6 @@
 import styled from "styled-components";
 
 const GameCardListStyled = styled.ul`
-  background: #1f2122;
-  color: white;
-  margin-top: 101px;
-  margin-left: 10px;
-  margin-right: 10px;
-  border-radius: 23px;
-  display: flex;
-  flex-wrap: wrap;
-  width: 95vw;
-  padding: 0;
-
-  .card-container__title {
-    font-size: 32px;
-    font-weight: 700;
-    text-decoration: underline;
-    padding-left: 40px;
-    padding-top: 40px;
-  }
-
   .card-container__cards {
     display: flex;
     justify-content: center;
