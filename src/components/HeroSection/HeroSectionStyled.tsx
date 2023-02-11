c9e5961fb41af3cce55cc72d19dda5c114cd5343
Marcel Martino <marcelmartino2053@gmail.com>
2023-02-11 20:37:56 +0100

    Create hero section component style it including media queries

---
+++ src/components/HeroSection/HeroSectionStyled.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const HeroSectionStyled = styled.section`
+  @media screen and (min-width: 800px) {
+    width: 100%;
+    height: 100%;
+    background-position: center;
+    background-size: 100%;
+    margin: 50px;
+    justify-content: space-around;
+    .herosection__text {
+      margin-top: 40px;
+      margin-bottom: 40px;
+    }
+  }
+  text-transform: uppercase;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 80px 60px;
+  margin-top: 50px;
+  width: 306px;
+  height: 520px;
+  border-radius: 23px;
+  background-image: url("https://templatemo.com/templates/templatemo_579_cyborg_gaming/assets/images/banner-bg.jpg");
+  background-position: 50%;
+  background-size: 480%;
+
+  .herosection__text {
+    text-align: center;
+    height: 100%;
+    font-weight: 700;
+    font-size: 35px;
+    line-height: 42px;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export default HeroSectionStyled;
